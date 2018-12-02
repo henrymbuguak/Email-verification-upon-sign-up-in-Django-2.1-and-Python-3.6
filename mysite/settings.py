4809ace3eb65e2768f0d9694fd1df12cbabb961e
henrymbuguak <henrymbuguak@gmail.com>
2018-12-02 17:19:15 +0300

    Settings changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'henrymbuguak@gmail.com'
-EMAIL_HOST_PASSWORD = '3918km1990hmk'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
